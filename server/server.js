f7a4532763998b62a5ba421686cae7e502ea4e7d
Your Name <you@example.com>
2026-06-16 13:05:32 +0300

    'updated'

--- server/server.js
+++ server/server.js
@@ -37,7 +37,12 @@ app.get('/', (req, res) => res.send('api is working success'));
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
+
+
+export default app

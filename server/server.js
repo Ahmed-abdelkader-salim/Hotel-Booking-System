bb4a877fa7523b01b39961d5324d2e71501adb7a
Your Name <you@example.com>
2026-06-16 13:31:15 +0300

    'updated'

--- server/server.js
+++ server/server.js
@@ -38,11 +38,11 @@ const PORT = process.env.PORT || 5000;
 
 
 if (process.env.NODE_ENV !== 'production') {
+    
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 }
-
 
 
 export default app
